11175f18c3dfa4a9cdf17c6d0f0e02434100a598
v-danyl <v-danyl@microsoft.com>
2016-09-28 15:30:56 +0800

    hugeint

---
+++ HugeInt/HugeInt/HugeIntDll.h
@@ -0,0 +1,81 @@
+﻿// HugeIntDll.h
+
+#ifdef HUGEINTDLL_EXPORTS
+#define HUGEINTDLL_API __declspec(dllexport) 
+#else
+#define HUGEINTDLL_API __declspec(dllimport) 
+#endif
+
+
+#define MAXLEN 1000
+#define POSITIVE 1
+#define NEGATIVE -1
+#define ZERO 0
+
+#include<iostream>
+using namespace std;
+namespace HugeIntSpace
+{
+	class HugeInt
+	{
+		friend HUGEINTDLL_API ostream &operator<<(ostream& output, const HugeInt& n);
+	public:
+		
+		HUGEINTDLL_API HugeInt(int = 0);
+		HUGEINTDLL_API HugeInt(char *str);
+		HUGEINTDLL_API HugeInt(const HugeInt& other);
+		HUGEINTDLL_API HugeInt& operator=(const HugeInt& other);
+
+		HUGEINTDLL_API bool operator==(const HugeInt &other)const;
+		HUGEINTDLL_API bool operator==(const int &x)const;
+		HUGEINTDLL_API bool operator!=(const HugeInt &other)const;
+		HUGEINTDLL_API bool operator!=(const int &x)const;
+
+		HUGEINTDLL_API bool operator>(const HugeInt &other)const;
+		HUGEINTDLL_API bool operator>(const int &x)const;
+		HUGEINTDLL_API bool operator>=(const HugeInt &other)const;
+		HUGEINTDLL_API bool operator>=(const int &x)const;
+
+		HUGEINTDLL_API bool operator<(const HugeInt &other)const;
+		HUGEINTDLL_API bool operator<(const int &x)const;
+		HUGEINTDLL_API bool operator<=(const HugeInt &other)const;
+		HUGEINTDLL_API bool operator<=(const int &x)const;
+
+
+		HUGEINTDLL_API HugeInt operator+(const HugeInt &other)const;
+		HUGEINTDLL_API HugeInt operator+(const int &x)const;
+		HUGEINTDLL_API HugeInt operator+=(const HugeInt &other);
+		HUGEINTDLL_API HugeInt operator+=(const int &x);
+		HUGEINTDLL_API HugeInt& operator++();//前置
+		HUGEINTDLL_API HugeInt operator++(int);//后置
+
+
+		HUGEINTDLL_API HugeInt operator-();//负号
+		HUGEINTDLL_API HugeInt operator-(const HugeInt &other)const;
+		HUGEINTDLL_API HugeInt operator-(const int &x)const;
+		HUGEINTDLL_API HugeInt operator-=(const HugeInt &other);
+		HUGEINTDLL_API HugeInt operator-=(const int &x);
+		HUGEINTDLL_API HugeInt& operator--();//前置
+		HUGEINTDLL_API HugeInt operator--(int);//后置
+
+		HUGEINTDLL_API HugeInt operator*(const HugeInt &other)const;
+		HUGEINTDLL_API HugeInt operator*(const int &x)const;
+		HUGEINTDLL_API HugeInt operator*=(const HugeInt &other);
+		HUGEINTDLL_API HugeInt operator*=(const int &x);
+
+		HUGEINTDLL_API HugeInt operator/(const HugeInt &other)const;
+		HUGEINTDLL_API HugeInt operator/(const int &x)const;
+		HUGEINTDLL_API HugeInt operator/=(const HugeInt &other);
+		HUGEINTDLL_API HugeInt operator/=(const int &x);
+
+		HUGEINTDLL_API HugeInt operator%(const HugeInt &other)const;
+		HUGEINTDLL_API int operator%(const int &x)const;
+		HUGEINTDLL_API HugeInt operator%=(const HugeInt &other);
+		HUGEINTDLL_API int operator%=(const int &x);
+
+		char num[MAXLEN];
+		int length;
+		int sign;
+		HUGEINTDLL_API bool isZero()const;
+	};
+}
---
+++ HugeInt/HugeInt_test/HugeInt_test.cpp
@@ -0,0 +1,56 @@
+﻿// HugeInt_test.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "HugeIntDll.h"
+#include <iostream>
+#include <fstream>
+
+using namespace HugeIntSpace;
+int main()
+{
+	ofstream fout;
+	fout.open("HugeInt_test.txt", ios::out);
+	////测试数据输入
+	HugeInt a("999999999999999999999"),b("8888765432101234"),c,d;
+	fout << a << " + " << b << " = " << a + b << endl;
+	fout << b << " - " << a << " = " << b - a << endl;
+	fout << a << " * " << b << " = " << a * b << endl;
+	fout << a << " / " << b << " = " << a / b << endl;
+	fout << a << " % " << b << " = " << a % b << endl;
+	c = a;
+	fout << "++" << c << " = " << ++a << endl;
+	c = a;
+	fout << "--" << c << " = " << --a << endl;
+	d = a;
+	c = a += b;
+	fout << d << " += " << b << " = " << c << endl;
+	d = a;
+	c = a -= b;
+	fout << d << " -= " << b << " = " << c << endl;
+	d = a;
+	c = a *= b;
+	fout << d << " *= " << b << " = " << c << endl;
+	d = a;
+	c = a /= b;
+	fout << d << " /= " << b << " = " << c << endl;
+	c = a > b;
+	d = a;
+	c = a %= b;
+	fout << d << " %= " << b << " = " << c << endl;
+	c = a > b;
+	fout << a << " > " << b << " = " << c << endl;
+	c = a < b;
+	fout << a << " < " << b << " = " << c << endl;
+	c = a >= b;
+	fout << a << " >= " << b << " = " << c << endl;
+	c = a <= b;
+	fout << a << " <= " << b << " = " << c << endl;
+	c = a == b;
+	fout << a << " == " << b << " = " << c << endl;
+	c = a != b;
+	fout << a << " != " << b << " = " << c << endl;
+	fout.close();
+    return 0;
+}
+
